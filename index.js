e50728788e6591130144438e2a8a309da8bc957e
Lava Ahmed <lavaahmed915@gmail.com>
2024-04-25 20:33:12 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution in this file!
+const distanceFromHqInBlocks = (pickUpLocation) => Math.abs(pickUpLocation - 42);
+
+const distanceFromHqInFeet = (pickUpLocation) => distanceFromHqInBlocks(pickUpLocation) * 264;
+
+const distanceTravelledInFeet = (start, destination) => Math.abs(start - destination) * 264;
+
+const calculatesFarePrice = (start, destination) => {
+  let distanceInFeet = distanceTravelledInFeet(start, destination);
+  if (distanceInFeet < 400) return 0;
+  else if (distanceInFeet <= 2000) return (distanceInFeet - 400) * 0.02;
+  else if (distanceInFeet <= 2500) return 25;
+  else return 'cannot travel that far';
+};
